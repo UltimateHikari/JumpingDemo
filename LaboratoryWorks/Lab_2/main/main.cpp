dd550d06014327d5583ed6761a4d7f21389d1bf0
UltimateHikari <andy@andystpad.localdomain>
2020-10-06 14:21:39 +0700

    cmake configuration

--- LaboratoryWorks/Lab_2/main/main.cpp
+++ LaboratoryWorks/Lab_2/main/main.cpp
@@ -35,3 +35,8 @@ int SDL_main(int argc, char **argv) {
     Visualizer::getInstance().draw();
     return 0;
 }
+
+int main(int argc, char **argv){
+    std::cout << "Hello World" << std::endl;
+    return 0;
+}

3759cd4583f63ea91189f7545afe71e57ad63fe4
UltimateHikari <andy@andystpad.localdomain>
2020-10-24 10:56:06 +0700

    random cube & MVP

--- LaboratoryWorks/Lab_3/src/main.cpp
+++ LaboratoryWorks/Lab_3/src/main.cpp
@@ -4,7 +4,11 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
 using namespace glm;
+
+#include <random>
+#include <time.h>
 
 #include "../engine/shader.hpp"
 
@@ -19,7 +23,6 @@ int main( void )
 	glfwWindowHint(GLFW_SAMPLES, 4);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // To make MacOS happy; should not be needed
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	window = glfwCreateWindow( 1024, 768, "Lab3", NULL, NULL);
 	if( window == NULL ){
@@ -39,7 +42,7 @@ int main( void )
 
 	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
 
-	glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
+	//glClearColor(0.0f, 0.0f, 0.2f, 0.0f);
 
 	GLuint VertexArrayID;
 	glGenVertexArrays(1, &VertexArrayID);
@@ -50,22 +53,87 @@ int main( void )
 		"../shaders/FShader.fragmentshader"
 		);
 
+	//generating matrices
+	mat4 projection = perspective(radians(45.0f), 4.0f/3.0f, 0.1f, 100.0f);
+	//mat4 projection = ortho(-5.0f,5.0f,-5.f,5.f,0.0f,10.0f);
+	mat4 view = lookAt(vec3(4,3,3), vec3(0,0,0), vec3(0,1,0));
+	mat4 model = mat4(1.0f); // E, no translation
+	mat4 mvp = projection * view * model; //backwards
 
-	static const GLfloat g_vertex_buffer_data[] = { 
-		-0.5f, -0.5f, 0.0f,
-		 0.5f, -0.5f, 0.0f,
-		 0.0f,  0.5f, 0.0f,
+	GLuint MatrixID = glGetUniformLocation(programID, "MVP");
+
+	// static const GLfloat g_vertex_buffer_data[] = { 
+	// 	-1.0f, -1.0f, 0.0f,
+	// 	 1.0f, -1.0f, 0.0f,
+	// 	 0.0f,  1.0f, 0.0f,
+	// };
+
+	static const GLfloat g_vertex_buffer_data[] = {
+		-1.0f,-1.0f,-1.0f, 
+		-1.0f,-1.0f, 1.0f,
+		-1.0f, 1.0f, 1.0f, 
+		1.0f, 1.0f,-1.0f, 
+		-1.0f,-1.0f,-1.0f,
+		-1.0f, 1.0f,-1.0f, 
+		1.0f,-1.0f, 1.0f,
+		-1.0f,-1.0f,-1.0f,
+		1.0f,-1.0f,-1.0f,
+		1.0f, 1.0f,-1.0f,
+		1.0f,-1.0f,-1.0f,
+		-1.0f,-1.0f,-1.0f,
+		-1.0f,-1.0f,-1.0f,
+		-1.0f, 1.0f, 1.0f,
+		-1.0f, 1.0f,-1.0f,
+		1.0f,-1.0f, 1.0f,
+		-1.0f,-1.0f, 1.0f,
+		-1.0f,-1.0f,-1.0f,
+		-1.0f, 1.0f, 1.0f,
+		-1.0f,-1.0f, 1.0f,
+		1.0f,-1.0f, 1.0f,
+		1.0f, 1.0f, 1.0f,
+		1.0f,-1.0f,-1.0f,
+		1.0f, 1.0f,-1.0f,
+		1.0f,-1.0f,-1.0f,
+		1.0f, 1.0f, 1.0f,
+		1.0f,-1.0f, 1.0f,
+		1.0f, 1.0f, 1.0f,
+		1.0f, 1.0f,-1.0f,
+		-1.0f, 1.0f,-1.0f,
+		1.0f, 1.0f, 1.0f,
+		-1.0f, 1.0f,-1.0f,
+		-1.0f, 1.0f, 1.0f,
+		1.0f, 1.0f, 1.0f,
+		-1.0f, 1.0f, 1.0f,
+		1.0f,-1.0f, 1.0f
 	};
+
+	srand(time(NULL));
+	static GLfloat g_color_buffer_data[12*3*3];
+	for(int i = 0; i < 12*3; ++i){
+		g_color_buffer_data[3*i] = (float)rand()/RAND_MAX;
+		g_color_buffer_data[3*i + 1] = (float)rand()/RAND_MAX;
+		g_color_buffer_data[3*i + 2] = (float)rand()/RAND_MAX;
+	}
+
 
 	GLuint vertexbuffer;
 	glGenBuffers(1, &vertexbuffer);
 	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
 
+	GLuint colorbuffer;
+	glGenBuffers(1, &colorbuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(g_color_buffer_data), g_color_buffer_data, GL_STATIC_DRAW);
+
+	glEnable(GL_DEPTH_TEST);
+	glDepthFunc(GL_LESS);
+
 	do{
-		glClear( GL_COLOR_BUFFER_BIT );
+		glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		glUseProgram(programID);
+		//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
 		glEnableVertexAttribArray(0);
 		glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
@@ -78,9 +146,24 @@ int main( void )
 			(void*)0            
 		);
 
-		glDrawArrays(GL_TRIANGLES, 0, 3); //Draw finally
+		glEnableVertexAttribArray(1);
+		glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
+		glVertexAttribPointer(
+			1,                  
+			3,                  
+			GL_FLOAT,           
+			GL_FALSE,           
+			0,                  
+			(void*)0            
+		);
+
+		glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &mvp[0][0]);
+
+
+		glDrawArrays(GL_TRIANGLES, 0, 3*12); //Draw finally
 
 		glDisableVertexAttribArray(0);
+		glDisableVertexAttribArray(1);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
